e968e95e2f2dce1d3c6d5f24a16163d116fbc0c5
Jeferson Marcante <jefemarcante@hotmail.com>
2024-01-05 23:16:33 -0300

    Desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Testes do Smartphone Nokia");
+Smartphone nokia = new Nokia("(12)123456789", "Modelo 01", "123456789", 12);
+Console.WriteLine($"Numero: {nokia.Numero}");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+
+Console.WriteLine("\nTestes do Smartphone Iphone");
+Smartphone iphone = new Iphone("(98)987654321", "Modelo 02", "987654321", 24);
+Console.WriteLine($"Numero: {iphone.Numero}");
+iphone.Ligar();
+nokia.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
